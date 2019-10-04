751e4e4f8efb251c92d3b0948657374e82330fe6
ylongyun <1464238094@qq.com>
2019-10-04 17:43:14 +0800

    test

---
+++ LinkList.h
@@ -0,0 +1,18 @@
+﻿#ifndef CORC___LINKLIST_H
+#define CORC___LINKLIST_H
+
+typedef struct Node
+{
+	int data;//ÄÚÈÝ
+	struct Node* next;//ÏÂÒ»¸ö½ÚµãµÄµØÖ·
+
+}LinkList;
+
+LinkList* creatList(int n);//
+int find_data(LinkList* list, int num);//
+void add(LinkList* head, int num);//
+void delete(LinkList* head, int num);//
+void print(LinkList* list);
+int size(LinkList* head);
+
+#endif //CORC___LINKLIST_H
