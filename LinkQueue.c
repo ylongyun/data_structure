3d5957b0227eb1568b86856989746401e756abe0
ylongyun <1464238094@qq.com>
2019-10-11 09:39:35 +0800

    增加了出队和入队操作

--- LinkQueue.c
+++ LinkQueue.c
@@ -16,3 +16,20 @@ void creatQueue(LinkQueue* queue)//初始化队列
 	queue->rear = queue->head;//此时队列没有成员，头部和尾部都指向头节点 
 	queue->head->next = NULL;//封闭队列 
 }
+viod enterQueue(LinkQueue* queue,int data)
+{
+	LinkQueueNode* node;
+	node = (LinkQueueNode*)malloc(sizeof(LinkQueueNode));
+	node->data = data;
+	queue->rear->next = node;
+	node->next = NULL;
+	queue->rear = node;
+ } 
+ void deleteQueue(LinkQueue* queue,int* data)
+ {
+ 	LinkQueueNode* p;
+ 	p = queue->head->next;
+ 	* data = p->data;
+ 	queue->head->next = p->next;
+ 	free(p);
+ }
