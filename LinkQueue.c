4bbe0554bb9ee3a81ff391ddc8327d611b2e76d9
ylongyun <1464238094@qq.com>
2019-10-13 19:05:32 +0800

    更新注释

--- LinkQueue.c
+++ LinkQueue.c
@@ -16,20 +16,20 @@ void creatQueue(LinkQueue* queue)//初始化队列
 	queue->rear = queue->head;//此时队列没有成员，头部和尾部都指向头节点 
 	queue->head->next = NULL;//封闭队列 
 }
-viod enterQueue(LinkQueue* queue,int data)
+viod enterQueue(LinkQueue* queue,int data)//入队操作 
 {
 	LinkQueueNode* node;
-	node = (LinkQueueNode*)malloc(sizeof(LinkQueueNode));
-	node->data = data;
-	queue->rear->next = node;
-	node->next = NULL;
-	queue->rear = node;
+	node = (LinkQueueNode*)malloc(sizeof(LinkQueueNode));//为新队员开辟空间 
+	node->data = data;//新队员的数据 
+	queue->rear->next = node;//将新节点插入队列 
+	node->next = NULL;//封闭队列 
+	queue->rear = node;//更新尾指针 
  } 
- void deleteQueue(LinkQueue* queue,int* data)
+ void deleteQueue(LinkQueue* queue,int* data)//出队操作 
  {
  	LinkQueueNode* p;
- 	p = queue->head->next;
- 	* data = p->data;
- 	queue->head->next = p->next;
- 	free(p);
+ 	p = queue->head->next;//找到第一个队员的地址 
+ 	* data = p->data;//取出队员的数据 
+ 	queue->head->next = p->next;//更新头指针 
+ 	free(p);//释放出队队员的空间 
  }
